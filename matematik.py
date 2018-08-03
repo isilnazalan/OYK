72a22e0898f64aaa627498d52c71be96ac4bc05f
isilnazalan <isilnazalan@icloud.com>
2018-08-03 11:34:52 +0300

    first commit

---
+++ matematik.py
@@ -0,0 +1,18 @@
+# buradaki * tüm kütüphaneyi çeker
+#from math import sqrt
+#math kütüphanesinin kullanımına, içindekilere bakmak için math. yapmalıyız
+#print(math.sqrt(16))
+#yarıçapı girilen dairenin çevresini ve alanını bulun
+from math import pi
+import math
+r =6
+alan = (math.pi)*r*r
+print(alan)
+#cevre = 2.(math.pi)*r
+#print(cevre)
+
+def uzaklik(*args):
+    uzunluk = (math.sqrt((x1-x2)+(y1-y2))
+    print(uzunluk)
+
+uzaklik(8,2,7,4)
